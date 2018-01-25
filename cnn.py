c18be838688e1fa92faf124aaa0c234b473f6af1
Yarlagadda <SYARLAG1@depaul.edu>
2018-01-24 19:50:22 -0600

    improved preprocess code + added CNN code

--- cnn.py
+++ cnn.py
@@ -1,2 +1,110 @@
 # runs a CNN to generate a model to predict the modality of the radiological images
 # written to run on AWS
+# Please note: Default name of data file is image_dict.pickle, modality_dict.pickle
+
+import os
+import numpy as np
+import pickle
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+# use keras a wrapper on tensorflow
+from keras.models import Sequential
+from keras.layers import Lambda, Dense, Flatten, Dropout, Activation
+from keras.layers.convolutional import Conv2D, Cropping2D
+from keras.layers.pooling import MaxPooling2D
+from keras.optimizers import Adadelta
+from keras.losses import categorical_crossentropy
+
+# set tensorflow as keras backend
+os.environ['KERAS_BACKEND'] = 'tensorflow'
+os.environ['image_data_format'] = 'channels_last'
+
+# define flags
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('data_dir','', "location of data folder")
+flags.DEFINE_string('save_dir','', "location to save model")
+flags.DEFINE_integer('batch_size', 128, "size of each batch")
+flags.DEFINE_integer('epochs', 50, "number of epochs to run the training")
+flags.DEFINE_float('learn_rate', 0.0001, "learning rate to use for training")
+
+if __name__ == '__main__':
+
+    os.chdir(FLAGS.data_dir)
+
+    # read in data:
+    with open('./image_dict.pickle') as f: X = pickle.load(f).values()
+    with open('./modality_dict.pickle') as f: y = pickle.load(f).values()
+
+    assert len(X) == len(y), "X,y size mismatch. Check data."
+
+    # prepare data
+    X_train, X_test, y_valid, y_valid = train_test_split(
+                                    X, y, stratify=y,
+                                    test_size=0.15)
+
+    #### create network graph ####
+    model = Sequential()
+    row, col, ch = 200, 300, 1 # of each image
+    num_classes = len(set(y))
+
+    # TODO: add some preprocessing layers
+    # convolutional layers:
+    # conv1 (need to specify input since it is first layer)
+    # NOTE: that we used same padding to have output of same size
+    model.add(Conv2D(filters=24, kernel_size=(5,5),
+                     input_shape=(row,col,ch),
+                     strides=(2,2), padding='same')
+    model.add(Activation('relu'))
+    # conv2
+    model.add(Conv2D(filters=36, kernel_size=(5,5),
+                     strides=(2,2), padding='same'))
+    model.add(Activation('relu'))
+    # conv3
+    model.add(Conv2D(filters=48, kernel_size=(5,5),
+                    strides=(1,1), padding='same'))
+    model.add(Activation('relu'))
+    # maxpooling
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(1,1),
+                           padding='same')
+    model.add(Activation('relu'))
+    # conv4
+    model.add(Conv2D(filters=36, kernel_size=(5,5),
+                    strides=(2,2), padding='valid'))
+    model.add(Activation('relu'))
+
+    # fully connected layers
+    model.add(Flatten())
+    # fully connected 1
+    model.add(Dense(500))
+    # Dropout
+    model.add(Dropout(0.1)) # prob of dropout
+    # fully connected 2
+    model.add(Dense(100))
+
+    # output layer (using softmax + cross entropy)
+    model.add(Dense(num_classes))
+    model.add(Activation('softmax'))
+    #### network graph is built ####
+
+    # create and run model
+    model.compile(loss=categorical_crossentropy,
+                  optimizer=Adadelta(),
+                  metric=['accuracy'])
+
+    model.fit(X_train, y_train,
+              batch_size=FLAGS.batch_size,
+              epochs=FLAGS.epochs,
+              verbose=True,
+              validation_data=(X_valid, Y_valid))
+
+    # print performance on test data
+    test_loss, test_acc = model.evaluate(X_test,
+                                         y_test, verbose=False)
+    print('Accuracy on test data: ', test_acc)
+    print('Cross Entropy Loss on test data: ', test_loss)
+
+    # save model for future use
+    model.save(FLAGS.save_dir + '/model.h5')
--- image_preprocess.py
+++ image_preprocess.py
@@ -2,25 +2,23 @@
 # then resizes all images to the same size
 
 import dicom
-import urllib
 import os
 from numpy import uint8, double, dot
 import argparse
 import pickle
-from scipy
+from scipy.misc import imresize
+import cv2
 
 parser = argparse.ArgumentParser(description='extracts images and attributes')
 parser.add_argument('-l', '--loc',  help='location of dicom images',
-                    const='C:/Users/syarlag1.DPU/Desktop/CBIR-for-Radiology/functions_for_database_CBIR/all_images_sample')
-parser.add_argument('-s', '--save',  help='(y/n) save dictionaries?', const='y', required=False)
+                    default='C:/Users/syarlag1.DPU/Desktop/CBIR-for-Radiology/functions_for_database_CBIR/all_images_sample')
+parser.add_argument('-s', '--save',  help='(y/n) save dictionaries?', default='y', required=False)
+parser.add_argument('-v', '--saveloc',  help='location to save dictionaries',
+                    default='C:/Users/syarlag1.DPU/Desktop/CBIR-for-Radiology', required=False)
 
 # returns resized image
 def resize_image(img, h_=200, w_=300):
     return imresize(img, (h_, w_))
-
-# convert to gray scale
-def gray(rgb):
-    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
 
 # returns extracted modality, body part, and pixel array for each dicom image
 # in the form of a dictionary: {image_id:(pixel_arr, modality)}
@@ -36,13 +34,13 @@ def extract_pixels_and_attributes(dicom_images_loc, resize=True, normalize=True,
 
     # iterate through each image in the folder
     for image_id in image_ids_list:
-
+        image_loc = dicom_images_loc + '/' + image_id
         try:
-            dicom_image_temp = dicom.read_file(image_id) # read as dicom
+            dicom_image_temp = dicom.read_file(image_loc) # read as dicom
+            print('Dicom read success:', image_id)
         except: # most likely due to non-dicom hidden files
-            print('failed:', image_id)
+            print('Dicom read failed:', image_id)
             continue
-
         try:
             pixel_array_ = dicom_image_temp.pixel_array
             if resize:
@@ -51,7 +49,12 @@ def extract_pixels_and_attributes(dicom_images_loc, resize=True, normalize=True,
                 temp_arr = double(pixel_array_)
                 pixel_array_ = uint8(255*((temp_arr - temp_arr.min()) / (temp_arr.max() - temp_arr.min())))
             if gray:
-                pixel_array_ = gray(rgb)
+                if len(pixel_array_.shape) == 3:
+                    # TODO: replace with something better
+                    # hacky way of dealing with images that have color channels first
+                    if pixel_array_.shape[0] == 3:
+                        pixel_array_ = pixel_array_.T
+                    pixel_array_= cv2.cvtColor(pixel_array_,cv2.COLOR_BGR2GRAY)
             image_pixel_dict[image_id] = pixel_array_
             image_modality_dict[image_id] = dicom_image_temp.Modality
             image_body_part_dict[image_id] = dicom_image_temp.BodyPartExamined
@@ -63,16 +66,18 @@ def extract_pixels_and_attributes(dicom_images_loc, resize=True, normalize=True,
     return fail_count, image_pixel_dict, image_modality_dict, image_body_part_dict
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     # read in the args
     args = vars(parser.parse_args())
     loc = args['loc']
     save = args['save']
+    save_loc = args['saveloc']
     # generate image, modality, and body part dicts
     fail_count, image_pixel_dict, image_modality_dict, image_body_part_dict =\
      extract_pixels_and_attributes(dicom_images_loc=loc)
-    if save:
+    if save == 'y':
         # save all three dicts
-        with f as open('image_dict.pickle','wb'): pickle.dump(image_pixel_dict, f)
-        with f as open('modality_dict.pickle','wb'): pickle.dump(image_modality_dict, f)
-        with f as open('body_part_dict.pickle','wb'): pickle.dump(image_body_part_dict, f)
+        os.chdir(save_loc)
+        with open('image_dict.pickle','wb') as f: pickle.dump(image_pixel_dict, f)
+        with open('modality_dict.pickle','wb') as f: pickle.dump(image_modality_dict, f)
+        with open('body_part_dict.pickle','wb') as f: pickle.dump(image_body_part_dict, f)
