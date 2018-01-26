6cca801f411a5107e0207f4ff3961e699340f41f
Yarlagadda <SYARLAG1@depaul.edu>
2018-01-25 18:40:06 -0600

    fixed minor error

--- cnn.py
+++ cnn.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
                                         test_size=0.1)
     X_train, X_valid, y_train, y_valid = train_test_split(
                                     X_train, y_train,
-                                    stratify=y, test_size=0.15)
+                                    stratify=y_train, test_size=0.15)
 
     #### create network graph ####
     model = Sequential()
